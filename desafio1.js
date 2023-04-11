01a09e016802f74a7d910d14932f4779f4e7233f
Franco <franco.ulzurrun.dmw@gmail.com>
2023-04-11 19:51:54 -0300

    validacion id

--- desafio1.js
+++ desafio1.js
@@ -4,42 +4,41 @@ class ProductManager {
         this.products = []
     }
 
-    addProduct = (title, description, price, thumbnail, code, stock) => {
-        let idBuscado = this.products.find(producto => producto.code == code)
-        if (idBuscado) {
-            let idModificado = code+1
-            return this.products.push({
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: thumbnail,
-                code: idModificado,
-                stock: stock
-            })
+    addProduct = (title, description, price, thumbnail, code, stock, id) => {
+        id = Math.floor(Math.random() * 5)
+        let codeBuscado = this.products.find(producto => producto.code == code)
+        if (codeBuscado) {
+            return console.log("Error! el codigo ya se ha utilizado")
         }
         else {
-            return this.products.push({
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: thumbnail,
-                code: code,
-                stock: stock
-            })
+            let idBuscado = this.products.find(producto => producto.id == id)
+            if (idBuscado) {
+                let idModificado = id + Math.floor(Math.random() * 100)
+                return this.products.push({
+                    title: title,
+                    description: description,
+                    price: price,
+                    thumbnail: thumbnail,
+                    code: code,
+                    stock: stock,
+                    id: idModificado,
+                })
+            }
+            else {
+                return this.products.push({
+                    title: title,
+                    description: description,
+                    price: price,
+                    thumbnail: thumbnail,
+                    code: code,
+                    stock: stock,
+                    id: id
+                })
+            }
+
         }
+
     }
-
-    /*validarId = (id) => {
-        let idBuscado = this.products.find(producto => producto.code == id)
-        if (idBuscado) {
-            let idModificado = id++
-            return (idModificado)
-        }
-        else {
-            return (id)
-        }
-
-    }*/
 
     getProducts = () => {
         return this.products
@@ -65,4 +64,4 @@ producto.addProduct("remera2", "nike2", 2000, "rutaimagen2", 1, 12)
 producto.addProduct("remera4", "nike4", 3000, "rutaimagen4", 2, 14)
 producto.addProduct("remera3", "nike3", 3000, "rutaimagen3", 3, 13)
 console.log(producto.getProducts())
-console.log(producto.getProductsById(2))
+console.log(producto.getProductsById(6))
